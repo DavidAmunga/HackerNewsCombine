1638ed88c2ccabc3e8495e9e70f5b8cb868e5368
David Amunga <david.onyimbo@gmail.com>
2021-05-24 19:53:10 +0300

    Initial Commit

---
+++ HackerNewsCombine/HackerNewsCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsCombineApp.swift
+//  HackerNewsCombine
+//
+//  Created by David Amunga on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
