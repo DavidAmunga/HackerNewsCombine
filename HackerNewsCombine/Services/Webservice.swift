79f2301b9b32e860dee42468f151ce7c970c5b85
David Amunga <david.onyimbo@gmail.com>
2021-05-24 19:57:46 +0300

    Initial Commit

---
+++ HackerNewsCombine/Services/Webservice.swift
@@ -0,0 +1,23 @@
+//
+//  Webservice.swift
+//  HackerNewsCombine
+//
+//  Created by David Amunga on 24/05/2021.
+//
+
+import Foundation
+import Combine
+
+class Webservice{
+    
+    func getAllTopStories()-> AnyPublisher<[Int],Error> {
+        guard let url = URL(string:
+                                "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")else{
+            fatalError("Invalid URL")
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Int].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
